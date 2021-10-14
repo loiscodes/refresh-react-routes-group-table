b223cb85114ce16e2def71136ff6adb9ca89f86b
Lois Harris <lois.harris@warnermedia.com>
2021-10-14 17:52:56 -0400

    first run

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import "./App.css";
-import GroupTableComponent from "./components/group-table.component";
-import GroupDetailsComponent from "./components/group-details.component";
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-        <Route exact path="/" component={GroupTableComponent} />
-        <Route exact path="/:groupId" component={GroupDetailsComponent} />
-        </Switch>
-      </Router>
+      <h1>My First React App</h1>
     </div>
   );
 }
